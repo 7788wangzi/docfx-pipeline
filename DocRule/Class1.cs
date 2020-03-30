9270bbb9019eab59332418c329809e44ad0a3889
Qijie Xue <qijiexue@outlook.com>
2020-03-30 18:16:03 +0800

    init files

---
+++ DocRule/Class1.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.DocAsCode.Plugins;
+using System;
+using System.Composition;
+
+namespace DocRule
+{
+    [Export("should_not_contain_onclick", typeof(ICustomMarkdownTagValidator))]
+    public class MyMarkdownTagValidator: ICustomMarkdownTagValidator
+    {
+       public bool Validate(string tag)
+        {
+            return tag.Contains("onclick");
+        }
+    }
+}
---
+++ DocRule/articles/intro.md
@@ -0,0 +1,11 @@
+# Introduction
+
+This file is the first markdown file.
+
+## Validate the markdown syntax
+
+<a href="https://www.google.com" onclick="a"/>
+
+<H2>Test invalid heading</H2>
+
+[Google](www.google.com)
